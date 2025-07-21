0b558bdc275d851acdfa1d60b0666184704b9676
Julian George <juliancgeorge@gmail.com>
2025-07-20 21:21:47 -0400

    fixing unsetting manual note

--- src/app/MainPage/Tab.tsx
+++ src/app/MainPage/Tab.tsx
@@ -110,7 +110,9 @@ export default function Tab({ tabKey }: TabProps) {
         stringNum,
         newFretNum == null
           ? null
-          : getNoteFromNumFrets(startingFretNotes[stringNum], newFretNum)
+          : getNoteFromNumFrets(newFretNum == 0 ?
+            stringTunings[stringNum] :
+            startingFretNotes[stringNum], newFretNum)
       );
     },
     [setManualStringNote, tabBaseNotes]
