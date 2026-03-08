788fbbbbe8754ffc5db16184c23d171525beeaa9
Julian George <juliancgeorge@gmail.com>
2026-03-08 16:35:01 -0400

    fixing song creation crashing bug

--- src/app/MainPage/MainPage.tsx
+++ src/app/MainPage/MainPage.tsx
@@ -149,9 +149,9 @@ export default function MainPage() {
 
   const orderedTabIds = useMemo(
     () =>
-      Object.values(song?.chords)
+      song?.chords ? Object.values(song?.chords)
         .sort((a, b) => a.index - b.index)
-        .map((chord) => chord.id),
+        .map((chord) => chord.id) : [],
     [song?.chords]
   );
 
@@ -184,211 +184,213 @@ export default function MainPage() {
         className="font-sans min-h-[92vh] flex flex-col items-center gap-4 grow px-2 md:pt-20 pt-12 box-border bg-[#fffefc]"
         id="main"
       >
-        <div className="centered-col gap-2">
-          <div className="centered-row gap-2 max-w-[66vw] flex-wrap">
-            <div className="flex flex-col min-w-16">
-              <span className="text-[12px]">String Tunings</span>
-              <div className="standard-input w-36 centered-row justify-start">
-                <AdaptiveInput
-                  value={stringifiedStringTunings}
-                  onChange={(newValue) => {
-                    // We split and rejoin so we can sanitize the notes
-                    setStringifiedStringTunings(
-                      newValue.target.value
-                        .split(",")
-                        .map(sanitizeNoteNameForLogic)
-                        .join(",")
-                    );
-                  }}
-                  onKeyDown={(e) => {
-                    const autocompleteKeys = new Set<string>([
-                      "Enter",
-                      "Tab",
-                      "Go",
-                      "Done",
-                    ]);
-                    if (autocompleteKeys.has(e.key)) {
-                      if (stringTuningsAutocompleteSuffix) {
+        {song ?
+          <>
+            <div className="centered-col gap-2">
+              <div className="centered-row gap-2 max-w-[66vw] flex-wrap">
+                <div className="flex flex-col min-w-16">
+                  <span className="text-[12px]">String Tunings</span>
+                  <div className="standard-input w-36 centered-row justify-start">
+                    <AdaptiveInput
+                      value={stringifiedStringTunings}
+                      onChange={(newValue) => {
+                        // We split and rejoin so we can sanitize the notes
+                        setStringifiedStringTunings(
+                          newValue.target.value
+                            .split(",")
+                            .map(sanitizeNoteNameForLogic)
+                            .join(",")
+                        );
+                      }}
+                      onKeyDown={(e) => {
+                        const autocompleteKeys = new Set<string>([
+                          "Enter",
+                          "Tab",
+                          "Go",
+                          "Done",
+                        ]);
+                        if (autocompleteKeys.has(e.key)) {
+                          if (stringTuningsAutocompleteSuffix) {
+                            autocompleteStringTunings();
+                            e.preventDefault();
+                          }
+                        }
+                      }}
+                      className="bg-transparent p-0"
+                      style={
+                        stringTuningsAutocompleteSuffix == ""
+                          ? { width: "100%" }
+                          : {}
+                      }
+                    />
+                    <span
+                      className="text-gray-500 cursor-pointer"
+                      onClick={(e) => {
                         autocompleteStringTunings();
                         e.preventDefault();
-                      }
+                      }}
+                    >
+                      {stringTuningsAutocompleteSuffix}
+                    </span>
+                  </div>
+                </div>
+                <div className="flex flex-col min-w-16">
+                  <span className="text-[12px]">Capo Fret</span>
+                  <input
+                    value={song?.capoFretNum ?? 0}
+                    onChange={(newValue) => {
+                      let parsedValue = parseInt(newValue.target.value);
+                      if (isNaN(parsedValue)) parsedValue = 0;
+                      if (parsedValue >= 0) setSongCapoFretNum(parsedValue);
+                    }}
+                    className="standard-input w-36"
+                  />
+                </div>
+                <div className="flex flex-col min-w-16">
+                  <span className="text-[12px]">Fret Window Size</span>
+                  <input
+                    value={song?.fretCount ?? 0}
+                    onChange={(newValue) => {
+                      let parsedValue = parseInt(newValue.target.value);
+                      if (isNaN(parsedValue)) parsedValue = 0;
+                      if (parsedValue <= 12 && parsedValue >= 0)
+                        setSongFretCount(parsedValue);
+                    }}
+                    type="number"
+                    className="standard-input w-36"
+                  />
+                </div>
+              </div>
+              <div className="centered-row">
+                <div className="flex flex-col max-w-[66vw]">
+                  <span className="text-[12px]">Chord Names</span>
+                  <MultiStringInput
+                    onChange={handleChordNamesChange}
+                    values={
+                      Object.entries(song?.chords || {}).reduce((acc, [id, chord]) => {
+                        acc[id] = {
+                          index: chord.index,
+                          value: chord.chordName,
+                        };
+                        return acc;
+                      }, {} as Record<string, { index: number; value: string }>) ||
+                      {}
                     }
-                  }}
-                  className="bg-transparent p-0"
-                  style={
-                    stringTuningsAutocompleteSuffix == ""
-                      ? { width: "100%" }
-                      : {}
-                  }
-                />
-                <span
-                  className="text-gray-500 cursor-pointer"
-                  onClick={(e) => {
-                    autocompleteStringTunings();
-                    e.preventDefault();
-                  }}
-                >
-                  {stringTuningsAutocompleteSuffix}
-                </span>
+                  />
+                </div>
               </div>
             </div>
-            <div className="flex flex-col min-w-16">
-              <span className="text-[12px]">Capo Fret</span>
-              <input
-                value={song?.capoFretNum ?? 0}
-                onChange={(newValue) => {
-                  let parsedValue = parseInt(newValue.target.value);
-                  if (isNaN(parsedValue)) parsedValue = 0;
-                  if (parsedValue >= 0) setSongCapoFretNum(parsedValue);
+            <div className="centered-col w-[80%] pb-32">
+              <div className="centered-row justify-between w-full pb-1">
+                <div className="centered-row gap-2">
+                  {Object.keys(songs).length > 0 && (
+                    <Select
+                      className="w-36 p-0"
+                      classNamePrefix="select"
+                      value={{
+                        value: songId,
+                        label: songId ? songs[songId]?.title : "New Song",
+                      }}
+                      onChange={(option) => {
+                        selectSong(option?.value ?? "");
+                      }}
+                      options={[
+                        { value: "", label: "New Song" },
+                        ...Object.entries(songs).map(([id, song]) => ({
+                          value: id,
+                          label: song.title,
+                        })),
+                      ]}
+                      styles={{
+                        control: (baseStyles, _state) => ({
+                          ...baseStyles,
+                          border: "none",
+                          boxShadow: "none",
+                          minHeight: "30px",
+                          "&:hover": {
+                            border: "none",
+                          },
+                        }),
+                        option: (baseStyles, state) => ({
+                          ...baseStyles,
+                          backgroundColor: state.isSelected
+                            ? "#e5e7eb"
+                            : state.isFocused
+                              ? "#f3f4f6"
+                              : "white",
+                          fontWeight: unsavedSongIds.has(state.data.value ?? "")
+                            ? "bold"
+                            : "normal",
+                          color: state.data.value === "" ? "#6B7280" : "#000000",
+                          "&:active": {
+                            backgroundColor: "#e5e7eb",
+                          },
+                        }),
+                        singleValue: (baseStyles, { data }) => ({
+                          ...baseStyles,
+                          color: data.value === "" ? "#6B7280" : "#000000",
+                          fontWeight: unsavedSongIds.has(data.value ?? "")
+                            ? "bold"
+                            : "normal",
+                        }),
+                        valueContainer: (baseStyles) => ({
+                          ...baseStyles,
+                          padding: "0px 0px",
+                          zIndex: 50,
+                        }),
+                        indicatorSeparator: () => ({
+                          display: "none",
+                        }),
+                        dropdownIndicator: (baseStyles) => ({
+                          ...baseStyles,
+                          color: "#6B7280",
+                        }),
+                        menu: (baseStyles) => ({
+                          ...baseStyles,
+                          border: "none",
+                          zIndex: 50,
+                        }),
+                      }}
+                    />
+                  )}
+                </div>
+                <div className="centered-row gap-2">
+                  {songId && isCurrentSongUnsaved && (
+                    <AiOutlineUndo
+                      className="cursor-pointer w-6 h-6 text-black"
+                      onClick={undoUnsavedChanges}
+                      title="Undo Unsaved Changes"
+                    />
+                  )}
+                  <AiOutlineSave
+                    className={`${isCurrentSongUnsaved || !songId
+                        ? "text-black"
+                        : "text-gray-500"
+                      } w-6 h-6 cursor-pointer`}
+                    onClick={songId ? onSave : openSongModal}
+                    title="Save Changes"
+                  />
+                  {songId && (
+                    <AiOutlineEdit
+                      className="text-black w-6 h-6 cursor-pointer"
+                      onClick={openSongModal}
+                      title="Edit Details"
+                    />
+                  )}
+                </div>
+              </div>
+              <div
+                className="w-full border-2 border-gray-300 border-solid rounded-md gap-8 px-8 py-4 grid justify-items-center"
+                style={{
+                  gridTemplateColumns: "repeat(auto-fit, minmax(12rem, 1fr))",
                 }}
-                className="standard-input w-36"
-              />
+              >
+                {orderedTabIds.map((id) => (
+                  <MemoizedTab key={id} tabId={id} />
+                ))}
+              </div>
             </div>
-            <div className="flex flex-col min-w-16">
-              <span className="text-[12px]">Fret Window Size</span>
-              <input
-                value={song?.fretCount ?? 0}
-                onChange={(newValue) => {
-                  let parsedValue = parseInt(newValue.target.value);
-                  if (isNaN(parsedValue)) parsedValue = 0;
-                  if (parsedValue <= 12 && parsedValue >= 0)
-                    setSongFretCount(parsedValue);
-                }}
-                type="number"
-                className="standard-input w-36"
-              />
-            </div>
-          </div>
-          <div className="centered-row">
-            <div className="flex flex-col max-w-[66vw]">
-              <span className="text-[12px]">Chord Names</span>
-              <MultiStringInput
-                onChange={handleChordNamesChange}
-                values={
-                  Object.entries(song.chords).reduce((acc, [id, chord]) => {
-                    acc[id] = {
-                      index: chord.index,
-                      value: chord.chordName,
-                    };
-                    return acc;
-                  }, {} as Record<string, { index: number; value: string }>) ||
-                  {}
-                }
-              />
-            </div>
-          </div>
-        </div>
-        <div className="centered-col w-[80%] pb-32">
-          <div className="centered-row justify-between w-full pb-1">
-            <div className="centered-row gap-2">
-              {Object.keys(songs).length > 0 && (
-                <Select
-                  className="w-36 p-0"
-                  classNamePrefix="select"
-                  value={{
-                    value: songId,
-                    label: songId ? songs[songId]?.title : "New Song",
-                  }}
-                  onChange={(option) => {
-                    selectSong(option?.value ?? "");
-                  }}
-                  options={[
-                    { value: "", label: "New Song" },
-                    ...Object.entries(songs).map(([id, song]) => ({
-                      value: id,
-                      label: song.title,
-                    })),
-                  ]}
-                  styles={{
-                    control: (baseStyles, _state) => ({
-                      ...baseStyles,
-                      border: "none",
-                      boxShadow: "none",
-                      minHeight: "30px",
-                      "&:hover": {
-                        border: "none",
-                      },
-                    }),
-                    option: (baseStyles, state) => ({
-                      ...baseStyles,
-                      backgroundColor: state.isSelected
-                        ? "#e5e7eb"
-                        : state.isFocused
-                        ? "#f3f4f6"
-                        : "white",
-                      fontWeight: unsavedSongIds.has(state.data.value ?? "")
-                        ? "bold"
-                        : "normal",
-                      color: state.data.value === "" ? "#6B7280" : "#000000",
-                      "&:active": {
-                        backgroundColor: "#e5e7eb",
-                      },
-                    }),
-                    singleValue: (baseStyles, { data }) => ({
-                      ...baseStyles,
-                      color: data.value === "" ? "#6B7280" : "#000000",
-                      fontWeight: unsavedSongIds.has(data.value ?? "")
-                        ? "bold"
-                        : "normal",
-                    }),
-                    valueContainer: (baseStyles) => ({
-                      ...baseStyles,
-                      padding: "0px 0px",
-                      zIndex: 50,
-                    }),
-                    indicatorSeparator: () => ({
-                      display: "none",
-                    }),
-                    dropdownIndicator: (baseStyles) => ({
-                      ...baseStyles,
-                      color: "#6B7280",
-                    }),
-                    menu: (baseStyles) => ({
-                      ...baseStyles,
-                      border: "none",
-                      zIndex: 50,
-                    }),
-                  }}
-                />
-              )}
-            </div>
-            <div className="centered-row gap-2">
-              {songId && isCurrentSongUnsaved && (
-                <AiOutlineUndo
-                  className="cursor-pointer w-6 h-6 text-black"
-                  onClick={undoUnsavedChanges}
-                  title="Undo Unsaved Changes"
-                />
-              )}
-              <AiOutlineSave
-                className={`${
-                  isCurrentSongUnsaved || !songId
-                    ? "text-black"
-                    : "text-gray-500"
-                } w-6 h-6 cursor-pointer`}
-                onClick={songId ? onSave : openSongModal}
-                title="Save Changes"
-              />
-              {songId && (
-                <AiOutlineEdit
-                  className="text-black w-6 h-6 cursor-pointer"
-                  onClick={openSongModal}
-                  title="Edit Details"
-                />
-              )}
-            </div>
-          </div>
-          <div
-            className="w-full border-2 border-gray-300 border-solid rounded-md gap-8 px-8 py-4 grid justify-items-center"
-            style={{
-              gridTemplateColumns: "repeat(auto-fit, minmax(12rem, 1fr))",
-            }}
-          >
-            {orderedTabIds.map((id) => (
-              <MemoizedTab key={id} tabId={id} />
-            ))}
-          </div>
-        </div>
+          </> : <></>}
       </main>
       <footer className="text-xs text-gray-500 px-2 gap-2 text-sm flex flex-row items-center h-[3vh] bg-[#fffefc]">
         <div>
--- src/app/MainPage/Modal.tsx
+++ src/app/MainPage/Modal.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import ReactModal from "react-modal";
+import * as Dialog from "@radix-ui/react-dialog";
 import { RxArrowLeft, RxCross1 } from "react-icons/rx";
 
 export type PageInfo = {
@@ -25,45 +25,38 @@ export default function Modal({
   children,
 }: ModalProps) {
   return (
-    <ReactModal
-      ariaHideApp={false}
-      isOpen={isOpen}
-      onRequestClose={closeModal}
-      style={{
-        overlay: {
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          zIndex: 100
-        },
-        content: {
-          width: "auto",
-          position: "relative",
-          inset: "0",
-        },
-      }}
-    >
-      <div className="centered-col w-[48rem] max-w-[80vw]">
-        <header className="flex flex-row justify-between w-full">
-          <div>
-            {onBack && (
-              <div
-                className="cursor-pointer flex flex-row items-center gap-1"
-                onClick={onBack}
-              >
-                <RxArrowLeft />
+    <Dialog.Root open={isOpen} onOpenChange={(open) => !open && closeModal()}>
+      <Dialog.Portal>
+        <Dialog.Overlay
+          className="fixed inset-0 z-[100] flex items-center justify-center bg-black/50"
+        >
+          <Dialog.Content
+            className="centered-col w-[48rem] max-w-[80vw] bg-white rounded p-4 relative"
+            onEscapeKeyDown={closeModal}
+            onPointerDownOutside={closeModal}
+          >
+            <header className="flex flex-row justify-between w-full">
+              <div>
+                {onBack && (
+                  <div
+                    className="cursor-pointer flex flex-row items-center gap-1"
+                    onClick={onBack}
+                  >
+                    <RxArrowLeft />
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-          <div className="cursor-pointer" onClick={closeModal}>
-            <RxCross1 />
-          </div>
-        </header>
-        <div className="w-full max-w-lg py-8">
-          {title && <div className="text-xl w-full py-1 font-medium">{title}</div>}
-          {children}
-        </div>
-      </div>
-    </ReactModal>
+              <div className="cursor-pointer" onClick={closeModal}>
+                <RxCross1 />
+              </div>
+            </header>
+            <div className="w-full max-w-lg py-8">
+              {title && <div className="text-xl w-full py-1 font-medium">{title}</div>}
+              {children}
+            </div>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
